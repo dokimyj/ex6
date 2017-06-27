4e1056df00f729905c540a98e18aa3bdaa4c92be
user2-PC\user2 <doublekick132@gmail.com>
2017-06-27 21:31:30 +0900

    about transaction exceptions(1)

---
+++ src/test/java/com/choa/ex6/TableTest.java
@@ -0,0 +1,29 @@
+package com.choa.ex6;
+
+import static org.junit.Assert.*;
+
+import javax.inject.Inject;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.choa.table.TableADTO;
+import com.choa.table.TableService;
+
+public class TableTest extends Abstract4Test {
+	
+	@Inject
+	private TableService tableService;
+	
+	@Test
+	public void test() throws Exception {
+		TableADTO tableADTO=new TableADTO();
+		tableADTO.setTitle("t2");
+		tableADTO.setWriter("t2");
+		tableADTO.setNum(2);
+		int result=tableService.insertTable(tableADTO, 1);
+		
+		Assert.assertNotEquals(0, result);
+	}
+
+}
